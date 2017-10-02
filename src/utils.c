374f6e11baeeb2be6454d488dc438823424b40a7
KSH <hol819@naver.com>
2017-10-02 21:44:03 +0900

    Do utils.c (1)

--- src/utils.c
+++ src/utils.c
@@ -1,7 +1,30 @@
 #include "utils.h"
 
-void mysh_parse_command(const char* command,
-                        int *argc, char*** argv)
-{
+void mysh_parse_command(const char* command, int *argc, char* argv[])
+{	
+	argc = 0;
+	const int comm = 1;
+	char* stack
+	for(int i=0; command[i] != '\0'; i++)
+	{
+		int k = 0;
+		
+		if(command[i] != ' ')
+		{
+			argv[argc][i] = command[i];			
+		}
+		else if(command[i] == ' ')
+		{	
+			argc++;
+		}
+	}
+	argc++;
+	printf("argc == [%d], argv == [ ",&argc);
+	for(int i=0; i < argc; i++)
+	{
+		printf("'%s' ",&argv[i]);
+	}
+	printf("]\n");
+	
   // TODO: Fill this!
 }
