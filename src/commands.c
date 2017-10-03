16b26001a0311201b1322e3e6149d8f7852d63bc
KSH <hol819@naver.com>
2017-10-03 22:51:18 +0900

    makeing ~ commands cd validate

--- src/commands.c
+++ src/commands.c
@@ -7,17 +7,14 @@ int do_cd(int argc, char** argv) {
     		return -1;
 
 	// TODO: Fill it!
-	else if(argc == 1)  
-		chdir(get("HOME"));
+	else if(argc == 1)
+		chdir(getenv("HOME"));
 	
 	else if(argc == 2)
 	{
 		if(chdir(argv[1])
-		printf("Can't go to %s",argv[1]);
+			printf("Can't go to %s",argv[1]);
 	}
-
-	else
-  		printf("please input cd [dir]\n");
 	
 	return 0;
 }
@@ -28,14 +25,25 @@ int do_pwd(int argc, char** argv)
 		return -1;
 	
 	// TODO: Fill it!
-	
-	
+
+	else
+	{
+		printf("%s\n", getenv("PWD"));
+	}
 	return 0;
 }
 
-int validate_cd_argv(int argc, char** argv) {
-  // TODO: Fill it!
-  return 1;
+int validate_cd_argv(int argc, char** argv) 
+{
+	if(argc > 2)
+	{
+		printf("please input cd [dir]\n");
+		return 0;
+	}	
+
+
+	// TODO: Fill it!
+	return 1;
 }
 
 int validate_pwd_argv(int argc, char** argv) {
